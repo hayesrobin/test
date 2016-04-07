57c90e986df953bde59587d2cf51b791047bc2f2
hayathasan <hayat.hasan@gmail.com>
2016-04-07 12:51:19 +0600

    example on php function return

---
+++ example1.php
@@ -0,0 +1,12 @@
+<?php
+	# Example 1
+	function addFunction($num1, $num2) {
+        $sum = $num1 + $num2;
+        return $sum;
+     }
+     $return_value = addFunction(10, 20);
+     
+     addFunction(10, 20);
+     echo "Returned value from the function : $return_value";
+     
+?>
---
+++ example2.php
@@ -0,0 +1,9 @@
+<?php
+	# Example 2
+     function addFunction($num1, $num2) {
+        $sum = $num1 + $num2;
+        echo $sum;
+     }
+     
+     addFunction(10, 20);
+?>
